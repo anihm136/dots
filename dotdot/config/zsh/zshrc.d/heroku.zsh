de322f4b7bb2cdf8498914dd990e2ca90f5013eb
Anirudh H M <anihm136@gmail.com>
2021-03-28 23:09:55 +0530

    [all] Add home variable instead of hardcoded home dir

--- dotdot/config/zsh/zshrc.d/heroku.zsh
+++ dotdot/config/zsh/zshrc.d/heroku.zsh
@@ -1,3 +1,3 @@
 
 # heroku autocomplete setup
-HEROKU_AC_ZSH_SETUP_PATH=/home/anihm136/.cache/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
+HEROKU_AC_ZSH_SETUP_PATH={{@@ home @@}}/.cache/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
