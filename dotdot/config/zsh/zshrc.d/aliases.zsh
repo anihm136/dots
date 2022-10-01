645bf0c8c05fb32178ba18134776ba9a587a399d
Anirudh H M <anihm136@gmail.com>
2022-10-01 20:30:00 +0530

    Fix dotdrop alias

--- dotdot/config/zsh/zshrc.d/aliases.zsh
+++ dotdot/config/zsh/zshrc.d/aliases.zsh
@@ -1,4 +1,4 @@
-alias dotdrop='(){(cd ~/.dots; pipenv run ./dotdrop.sh $@) ;}'
+alias dotdrop='(){(cd ~/.dots || exit; pipenv run ./dotdrop.sh $@) ;}'
 
 alias -g '...'='../..'
 alias -g '....'='../../..'
