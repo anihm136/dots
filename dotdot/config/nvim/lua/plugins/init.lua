32fa4215766402f59d52b563c2d5bd4bc1eaa00e
Anirudh H M <anihm136@gmail.com>
2023-01-14 21:04:18 +0530

    Configure many things to use rose pine

--- dotdot/config/nvim/lua/plugins/init.lua
+++ dotdot/config/nvim/lua/plugins/init.lua
@@ -3,6 +3,6 @@ require("plugins.bootstrap")
 require("lazy").setup("plugins.install", {
 	defaults = { lazy = true },
 	install = {
-		colorscheme = { "catppuccin", "habamax" },
+		colorscheme = { "rose-pine", "habamax" },
 	},
 })
--- dotdot/config/zsh/zshrc.d/env.zsh
+++ dotdot/config/zsh/zshrc.d/env.zsh
@@ -4,13 +4,6 @@ export PIPENV_VENV_IN_PROJECT=1
 # Use bat to highlight manpages
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
-# FZF settings
-export FZF_DEFAULT_OPTS="--height 40% --reverse"
-export FZF_CTRL_T_OPTS="--preview '(bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -500' --preview-window right:60% --select-1"
-export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
-export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git --exclude node_modules --exclude .venv --exclude .ccls-cache --exclude .steam/"
-export FZF_CTRL_T_COMMAND="fd --hidden --follow --exclude .git --exclude node_modules --exclude .venv --exclude .ccls-cache --exclude .steam/"
-
 # Don't print logs when changing into directory
 export DIRENV_LOG_FORMAT=
 
